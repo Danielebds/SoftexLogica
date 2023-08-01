81aed16323ffa6daf8c6115026bb1318f7a7b3e8
Daniele Barbosa <bdsdaniele22@gmail.com>
2023-08-01 16:51:55 -0300

    tarefa

--- LogicaJavaScript/atividades04.js
+++ LogicaJavaScript/atividades04.js
@@ -56,3 +56,38 @@ if (!isNaN (numDeci)) {
 } else {
     console.log("número invalido")
 }
+
+//Peça ao usuário para digitar dois números inteiros e exiba a soma deles.
+
+let num1 =prompt ("Digite um número Inteiro");
+let num2 =prompt("Digite o segundo número Inteiro");
+
+let resultado = Number (numero1) + Number (numero2);
+
+alert(resultado)
+
+//Receba um número decimal do usuário e calcule o seu quadrado.
+
+let NumDeci = parseFloat (prompt("Digite um número Decimal:"));
+
+const quadrado = NumDeci * NumDeci;
+
+alert(`O quadrado de ${NumDeci} é ${quadrado}`);
+
+//Peça ao usuário que insira o seu ano de nascimento e, em seguida, exiba a sua idade.
+
+
+let nascimento = prompt("Digite seu ano de Nascimento: ")
+let anoAtual = new Date().getFullYear();
+const idade = anoAtual - parseInt(nascimento)
+
+alert(`Sua idade é ${nascimento} - ${anoAtual} = - ${idade}`);
+
+/*Peça ao usuário que digite seu primeiro nome e seu sobrenome separadamente. Em seguida,
+concatene-os em uma única string e exiba o nome completo.*/
+
+const nome = prompt("Digite seu Nome: ")
+const sobrenome = prompt("Digite seu Sobrenome: ")
+
+alert (`-- ${nome} ${sobrenome}`);
+
---
+++ LogicaJavaScript/index.js
@@ -0,0 +1,6 @@
+/*Solicite ao usuário uma sequência de números separados por espaço e exiba quantos números
+foram digitados.*/
+
+let numero = [
+    
+]
