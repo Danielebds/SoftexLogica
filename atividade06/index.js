1b0f03a6d779d78dc3e7cd4a05249b40cfcd67ca
Daniele Barbosa <bdsdaniele22@gmail.com>
2023-08-08 16:38:59 -0300

    atividade06

--- atividade06/index.js
+++ atividade06/index.js
@@ -1,6 +1,6 @@
-//Desenvolva um programa que exiba todos os números pares de 1 a 50.
+//Desenvolva um programa que exiba todos os números impares de 1 a 50.
 
-for (let index = 0; index < 50; index = index + 2) {
-    console.log(index);
+for (let index = 2; index < 50; index = index + 2) {
+    console.log(index - 1);
 
 }
