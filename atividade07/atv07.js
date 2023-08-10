cf2f7a670f1623609e9c032d0324aa64bca3afaa
Daniele Barbosa <bdsdaniele22@gmail.com>
2023-08-09 22:48:10 -0300

    atv07

---
+++ atividade07/atv07.js
@@ -0,0 +1,34 @@
+/*01-Enumere as diferenças entre os comandos "for" e "while".
+R - (For ) Usado quando o número de repetições é conhecido
+R - (While) Usado quando o número de iterações não é conhecido*/
+
+/*02-Descreva em quais circunstâncias os comandos "for" e "while" devem ser empregados.
+ R - (For ) Usado para repetições com contagem conhecida.
+ R - (While) Utilizado para repetições baseadas em condições de término complexas ou variáveis.*/
+
+//Implemente o código do slide de número 9.
+
+let i = 0;
+while (i < 10) {
+    console.log("testando uma frase!")
+    i++;
+}
+
+//Implemente o código do slide de número 16.
+ 
+let i = 465484133;
+while (i > 10) {
+    console.log(i);
+
+    i/=35;
+}
+
+//Implemente o código do slide de número 26.
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);
+
+
